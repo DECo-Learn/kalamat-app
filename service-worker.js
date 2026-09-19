5264dab8db93d912ce73efd52b98ea6f2d3813c5
hosnakhorsandi-create <hosna.khorsandi@gmail.com>
2026-09-19 13:26:37 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,43 @@
-
-const CACHE = 'kalamat-v1';
-const FILES = [
- './',
- './index.html',
- './manifest.json'
+const CACHE = 'kalamat-v4-teal-brothers';
+const ASSETS = [
+  './',
+  './index.html?v=4',
+  './manifest.json',
+  './icon-192.png?v=4',
+  './icon-512.png?v=4',
+  './apple-touch-icon.png?v=4'
 ];
 
-self.addEventListener('install', e => {
- e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
- e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
- );
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put('./index.html?v=4', copy));
+        return resp;
+      }).catch(() => caches.match('./index.html?v=4'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(cache => cache.put(req, copy));
+      return resp;
+    }))
+  );
 });
